f3dd952985a95335e9169838d31561e01d9a0238
temal07 <temiralimov38@gmail.com>
2024-06-05 15:38:11 -0400

    change index.js without dist

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use('/api/activity', activityRoute);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(process.cwd(), 'client', 'index.html'));
 });
 
 // create an error handler middleware
